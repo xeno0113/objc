8b1bea8b0573f01d164c1cf78a222024f9342b5e
Hyojong Ha <xeno0113@naver.com>
2016-05-24 11:01:58 +0900

    Initial Commit

---
+++ ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC
+//
+//  Created by 하효종 on 2016. 5. 24..
+//  Copyright © 2016년 Hyojong Ha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
